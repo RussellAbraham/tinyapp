b5081d519c90c0b2e9bf09d34b6390bd3c79148e
RussellAbraham <russell_a@live.com>
2023-06-23 18:17:03 -0700

    add templates and update server

--- express_server.js
+++ express_server.js
@@ -24,11 +24,23 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
+
+app.get("/urls", (req,res) => {
+  console.log(req.params);
+  const templateVars = { urls : urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const key = req.params.id;
+  const templateVars = { id: key, longURL: urlDatabase[key] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
