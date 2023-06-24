470fa008db12b0dbd03625ebb127d1f02ede8e7f
RussellAbraham <russell_a@live.com>
2023-06-23 20:29:58 -0700

    add get and post routes for new urls, add helper function to generate string for short urls

--- express_server.js
+++ express_server.js
@@ -3,11 +3,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function() {
+  const length = 6;
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+};
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,23 +30,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
 app.get("/urls", (req,res) => {
   res.locals.title = "TinyApp";
   const templateVars = { urls : urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.locals.title = "New URL - TinyApp Example";
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -41,6 +46,11 @@ app.get("/urls/:id", (req, res) => {
   const key = req.params.id;
   const templateVars = { id: key, longURL: urlDatabase[key] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
